2eff91c2180d2d3f62eb287d3e4709dffd541632
r.shi <r.shi@trip.com>
2023-03-12 18:20:24 +0800

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,16 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const simplePlantUML = require("@akebifiky/remark-simple-plantuml");
-
+// theme
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// plantuml
+const simplePlantUML = require("@akebifiky/remark-simple-plantuml");
+
+// math-equations 
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -45,7 +51,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          remarkPlugins: [simplePlantUML],
+          remarkPlugins: [simplePlantUML, math],
+          rehypePlugins: [katex],
           editUrl:
             'https://github.com/shirayner/notebook/tree/master/',
         },
@@ -62,6 +69,21 @@ const config = {
       }),
     ],
   ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
